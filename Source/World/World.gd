f2a98078aee20b8ac4e2e2da8c5d084c639e0594
Henrik Markengbakken Karlsen <henrikmk@stud.ntnu.no>
2021-09-24 12:32:16 +0200

    [32] In progress

--- Source/World/World.gd
+++ Source/World/World.gd
@@ -25,17 +25,25 @@ func createboxes():
 	for i in 3:
 		boxes = box.instance()
 		boxes.get_node("Sprite").set_texture(whole_box)
+		var height = boxes.get_node("Sprite").texture.get_height()
+		var width = boxes.get_node("Sprite").texture.get_width()
+		print(height)
+		print(width)
+		var shape = RectangleShape2D.new()
+	
+		boxes.position = Vector2(pos[i],pos[i+1]) # Just some shit placement code
+		boxes.get_node("CollisionShape2D").position = boxes.get_node("Sprite").position
+		#boxes.get_node("CollisionShape2D").extents = (Vector2(width, height))
 		add_child(boxes)
 		print(boxes.name)
 		listboxes.push_back(boxes)
-		boxes.position = Vector2(pos[i],pos[i+1]) # Just some shit placement code
 
 func _unhandled_input(event):
 	if event is InputEventKey:
-		if event.pressed and event.scancode == KEY_S:
-			#tex.create_from_image(image)
+		if event.pressed and event.scancode == KEY_P:
 			for list in listboxes:
 				list.get_node("Sprite").set_texture(image)
+				list.get_node("CollisionShape2D").disabled = true
 				
 			
 			

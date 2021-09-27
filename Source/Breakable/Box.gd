bffb3266c30f374733b06c3174dd43c11043126e
Henrik Markengbakken Karlsen <henrik.mk@icloud.com>
2021-09-27 13:12:05 +0200

    added collision health to boxes which decreases when it gets shot.

--- Source/Breakable/Box.gd
+++ Source/Breakable/Box.gd
@@ -1,14 +1,30 @@
-extends Node
+extends Area2D
 
 var whole_box = preload("res://Assets/Spritesheet/BreakableObj/broken_box.png")
+export (float) var max_health = 100
+onready var health = max_health
 
 func destroybox():
 	get_node("Sprite").set_texture(whole_box)
-	get_node("CollisionShape2D").disabled = true
+	$CollisionShape2D.set_deferred("disabled", true)
 	print("Destroybox")
-				
-			
-			
-			
+
+func take_damage(amount):
+	# Reduce health by given amount
+	health = clamp(health - amount, 0, max_health)
 	
+	#Destroys the box
+	if health == 0:
+		destroybox()
+		
+# Checks if the box is already broken
+func is_broken():
+	if $CollisionShape2D.disabled == true:
+		return true
+	return false
+	
+	
+	
+
+
 

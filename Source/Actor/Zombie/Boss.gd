506d5be63f761a5a7a5bc730b80c60681b49c147
sindre0830 <sindre0830@gmail.com>
2021-10-14 17:28:14 +0200

    [#37] Add hitbox to boss

--- Source/Actor/Zombie/Boss.gd
+++ Source/Actor/Zombie/Boss.gd
@@ -1,6 +1,13 @@
 extends "res://Actor/Enemy.gd"
 
 onready var navigation = get_parent().get_node("Navigation2D")
+	
+func _ready():
+	timer = Timer.new()
+	timer.set_wait_time(1.0)
+	timer.set_one_shot(false)
+	add_child(timer)	
+	timer.connect("timeout", self, "_on_timer_timeout")
 
 func _physics_process(delta) -> void:
 	move(delta, global_position, navigation)

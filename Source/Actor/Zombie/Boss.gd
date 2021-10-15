92a5472366ba8ebf5de3b35f64dfef95ae6f29a5
sindre0830 <sindre0830@gmail.com>
2021-10-15 09:41:27 +0200

    [#39] Connected healthbar to boss

--- Source/Actor/Zombie/Boss.gd
+++ Source/Actor/Zombie/Boss.gd
@@ -14,8 +14,14 @@ func _ready():
 	max_health = get_node("Stats").max_health
 	health = get_node("Stats").max_health
 	MAX_SPEED = 150
+	self.get_node("CanvasLayer/ProgressBar").max_value = max_health
+	self.get_node("CanvasLayer/ProgressBar").value = health
 
 func _physics_process(delta) -> void:
 	move(delta, global_position, navigation)
 	if !flagDamageModified && health < max_health / 2:
 		damage = 30
+
+func take_damage(amount):
+	.take_damage(amount)
+	self.get_node("CanvasLayer/ProgressBar").value = health

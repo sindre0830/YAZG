0f35b857febc1cf58e1b2e82a5caf6fbd254a893
Martin Araya <martin.araya.zavala@gmail.com>
2021-10-15 11:35:20 +0200

    changed to 1 portal with exported variable

--- Source/Portals/Portal.gd
+++ Source/Portals/Portal.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-
+export var path: String 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _on_body_entered(body: Node) -> void:
-	get_tree().change_scene("res://World/World2.tscn")
+	get_tree().change_scene(path)

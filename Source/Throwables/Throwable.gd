04fa579ec5d7baeabcc7055776ec0683423b8eba
Niklas Menzel <niklas.menzel@gmx.net>
2021-10-22 09:38:48 +0200

    Add throwable Frag Grenade

---
+++ Source/Throwables/Throwable.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+# How fast should the object slow down
+var deceleration
+
+func _physics_process(delta):
+	# Current speed is the length of the velocity vector
+	var speed = self.linear_velocity.length()
+	
+	# Decrease speed according to elapsed time
+	speed -= deceleration * delta
+	
+	# Speed can't be smaller than 0
+	if speed < 0.0:
+		speed = 0.0
+		
+	# Slow down the throwable item
+	self.linear_velocity = self.linear_velocity.normalized() * speed

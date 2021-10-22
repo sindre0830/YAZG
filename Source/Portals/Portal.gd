b24383d9bbf6f59b00f9dea895c5d598446a8de5
Martin Araya <martin.araya.zavala@gmail.com>
2021-10-22 10:30:21 +0200

    trying to save the world as a temp file

--- Source/Portals/Portal.gd
+++ Source/Portals/Portal.gd
@@ -1,29 +1,32 @@
 extends Area2D
 
 export var path: String 
+export var destination_position: Vector2
 
+var directory = Directory.new();
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _on_body_entered(body: Node) -> void:	
-	var scene = PackedScene.new()
-	print(PreviousWorld.previous_name)
-	print(path)
-	print("current path:" + get_tree().current_scene.filename)
-	if(PreviousWorld.previous_name != path):
-		print("new saved")
-		scene.pack(get_tree().get_root())
-		PreviousWorld.previous_world = scene
-		PreviousWorld.previous_name = get_tree().current_scene.filename
-		get_tree().change_scene(path)
-	else:
+	print("destination: " + path)
+	print("current path: " + get_tree().current_scene.filename)
+	print("temp path: " + "res://Temp/temp_" + get_tree().current_scene.filename.split("/")[3])
+	save_current_world()
+	if(directory.file_exists("res://Temp/temp_" + path.split("/")[3])):
 		print("loading previous")
-		var new_scene = PreviousWorld.previous_world.instance()
-		new_scene.owner = get_tree().get_root()
-		scene.pack(get_tree().get_root())
-		PreviousWorld.previous_world = scene
-		PreviousWorld.previous_name = get_tree().current_scene.filename
-		get_tree().change_scene_to(new_scene)
+		get_tree().change_scene("res://Temp/temp_" + path.split("/")[3])
+	else:
+		print("new saved")
+		get_tree().change_scene(path)
+	print("do i get here?")
+	print(body)
+	print(get_owner().get_node("Player"))
 	
+	
+func save_current_world()-> void:
+	var file_path = "res://Temp/temp_" + get_tree().current_scene.filename.split("/")[3]
+	var scene = PackedScene.new()
+	scene.pack(get_tree().current_scene)
+	ResourceSaver.save(file_path,scene)

0b6f1e9c9b9be3661c2506566e09d3d63601be5e
caspertm <caspertm@stud.ntnu.no>
2021-10-22 08:22:57 +0200

    [#58] Made inital MVP map

--- Source/Menu/MainMenu.gd
+++ Source/Menu/MainMenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_Play_pressed():
 	# Start the game by switching to the World scene
-	assert(get_tree().change_scene("res://World/TutorialScene.tscn") == OK)
+	assert(get_tree().change_scene("res://World//MVP_World//1_1.tscn") == OK)
 
 func _on_ExitGame_pressed():
 	# Quit the game

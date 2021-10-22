1c16d06fc8bf258ea5d87ea4f9cbe66b620cd205
Henrik Markengbakken Karlsen <henrik.mk@icloud.com>
2021-10-22 09:47:47 +0200

    [77]Added functionality to the Iron Door so it cant be destroyed untill there are no zombies left

--- Source/Breakable/WoodDoor.gd
+++ Source/Breakable/WoodDoor.gd
@@ -1,10 +1,11 @@
 extends "res://Breakable/BreakableObject.gd"
 
 func _init():
-	max_health = 200
+	max_health = 100
 	health = max_health
 
 
 func destroy_object():
 	# Remove car
 	queue_free()
+

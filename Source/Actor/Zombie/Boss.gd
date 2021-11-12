fc97899331b1b27f68a44325e8c5825ffc4c05f3
sindre0830 <sindre0830@gmail.com>
2021-11-12 08:58:28 +0100

    [#114] Add different modes to urban boss

--- Source/Actor/Zombie/Boss.gd
+++ Source/Actor/Zombie/Boss.gd
@@ -1,8 +1,14 @@
 extends "res://Actor/Enemy.gd"
 
-var flagDamageModified = false
 var collided = false
 const CHASE_TOLERANCE = 50.0
+var mode = NORMAL
+
+enum {
+	NORMAL,
+	INSANE,
+	SPRINT
+}
 
 func _ready():
 	timer = Timer.new()
@@ -11,21 +17,31 @@ func _ready():
 	add_child(timer)	
 	timer.connect("timeout", self, "_on_timer_timeout")
 	#change stats
-	damage = 20
 	max_health = get_node("Stats").max_health
 	health = get_node("Stats").max_health
-	MAX_SPEED = 150
 	self.get_node("CanvasLayer/ProgressBar").max_value = max_health
 	self.get_node("CanvasLayer/ProgressBar").value = health
 
 func _physics_process(delta) -> void:
 	var path = player.global_position
+	match mode:
+		NORMAL:
+			damage = 15
+			MAX_SPEED = 200
+			modulate = Color(0, 0, 0)
+		INSANE:
+			damage = 30
+			MAX_SPEED = 250
+			modulate = Color(0.1, 0, 0)
+		SPRINT:
+			damage = 10
+			MAX_SPEED = 350
+			modulate = Color(0, 0, 0.3)
 	if (collided && collided.collider != null) && !("Zombie" in collided.collider.name || "Boss" in collided.collider.name) && (path - global_position).length() > CHASE_TOLERANCE:
 		path = getNextPosition(global_position, path)
 	collided = move(delta, path, 0.2)
-	if !flagDamageModified && health < max_health / 2:
-		damage = 30
-		flagDamageModified = true
+	if health < max_health / 2:
+		mode = INSANE
 	# Boss death animation
 	if health < max_health/3 && max_health/5 > health:
 		modulate = Color(0.4, 0, 0)
@@ -36,3 +52,14 @@ func _physics_process(delta) -> void:
 func take_damage(amount):
 	.take_damage(amount)
 	self.get_node("CanvasLayer/ProgressBar").value = health
+
+
+func _on_modeSwitchTimer_timeout():
+	if mode != SPRINT:
+		mode = SPRINT
+		$modeSwitchTimer.wait_time = 4
+		$modeSwitchTimer.start()
+	else:
+		mode = NORMAL
+		$modeSwitchTimer.wait_time = 8
+		$modeSwitchTimer.start()

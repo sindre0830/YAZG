b29810da91281c38beaff155a8c53b93ae8d3ca3
Henrik Markengbakken Karlsen <henrik.mk@icloud.com>
2021-11-12 12:47:45 +0100

    Added code for saving only highest score as highscore

--- Source/Menu/DeathScreen.gd
+++ Source/Menu/DeathScreen.gd
@@ -7,8 +7,8 @@ onready var score = 0
 func _ready():
 	score = PlayerValues.XP /  PlayerValues.time_end
 	get_node(label).set_text(get_node("Label").get_text() + " with points: " + str(score))
-	
-	save(score)
+	if score > PlayerValues.read_savegame():
+		save(score)
 
 
 
@@ -31,11 +31,8 @@ func _on_RTMainMenu_pressed():
 	# Switch scene back to main menu
 	assert(get_tree().change_scene("res://Menu/MainMenu.tscn") == OK)
 
-
 	
-
-	
-func save(high_score):    
+func save(high_score):     # Saves he high score
    PlayerValues.save_data["highscore"] = high_score #data to save
    PlayerValues.savegame.open(PlayerValues.save_path, File.WRITE) #open file to write
    PlayerValues.savegame.store_var(PlayerValues.save_data) #store the data

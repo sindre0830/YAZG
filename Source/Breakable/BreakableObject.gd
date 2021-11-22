4be8398a2a6f13c507d8f80c4b556a473ffa3804
Henrik Markengbakken Karlsen <henrik.mk@icloud.com>
2021-11-22 13:22:37 +0100

    Fixed iron doors and added XP UI and restructured XP code

--- Source/Breakable/BreakableObject.gd
+++ Source/Breakable/BreakableObject.gd
@@ -22,12 +22,3 @@ func is_broken():
 		return true
 	return false
 
-func _process(delta):
-		# Remove car
-	var isZombie = get_owner().get_children()
-	var zombiecount = 0
-	for i in isZombie:
-		if "Zombie" in i.get_name():
-			zombiecount += zombiecount + 1
-	if zombiecount == 0: 
-		destroy_object()

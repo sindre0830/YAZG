c49a7017f02748093360339178bb9b00484b7634
Henrik Markengbakken Karlsen <henrik.mk@icloud.com>
2021-11-30 14:35:41 +0100

    Fixed iron door issue to also check if there are other zombietypes

--- Source/Breakable/IronDoor.gd
+++ Source/Breakable/IronDoor.gd
@@ -5,7 +5,9 @@ func _process(delta):
 	var isZombie = get_parent().get_children()
 	var zombiecount = 0
 	for i in isZombie:
-		if "Zombie" in i.get_name():
+		if ("Zombie" in i.get_name() || "Spitter" in i.get_name() 
+		|| "Acid" in i.get_name()    || "Bomber"  in i.get_name() 
+		|| "Boss" in i.get_name()):
 			zombiecount += zombiecount + 1
 	if zombiecount == 0: 
 		queue_free()
